e68b14269f8785b01a1ab59e75d979fedcc9754f
Nadim-Shahoud <ndmshahoud@gmail.com>
2022-12-21 17:34:53 +0200

    own lexer

--- stages/lexer/lexer.py
+++ stages/lexer/lexer.py
@@ -1,85 +1,88 @@
 
-class SyntaxKind:
-    NumberToken = "NumberToken"
-    WhiteSpaceToken = "WhiteSpaceToken"
-    PlusToken = "PlusToken"
-    MinusToken = "MinusToken"
-    StarToken = "StarToken"
-    SlashToken = "SlashToken"
-    OpenParenthesisToken = "OpenParenthesisToken"
-    ClosedParenthesisToken = "ClosedParenthesisToken"
+class TokenTypes:
+    Int = "Int"
+    Float = "Float"
+    WhiteSpace = "WhiteSpace"
+    Plus = "Plus"
+    Minus = "Minus"
+    Asterisk = "Asterisk"
+    Slash = "Slash"
+    LParenthesis = "LParenthesis"
+    RParenthesis = "RParenthesis"
     BadToken = "BadToken"
-    EndOfFileToken = "EndOfFileToken"
-    BinaryExpression = "BinaryExpression"
-    NumberExpressionSyntax = "NumberExpressionSyntax"
 
-class SyntaxToken:
-    def __init__(self, kind:SyntaxKind, position:int, text:str, value):
-        self.kind = kind
+class Token:
+    def __init__(self, position, type_, text, value=None):
         self.position = position
+        self.type = type_
         self.text = text
         self.value = value
 
 class Lexer:
     def __init__(self, text):
-        self.text: str = text
-        self.__position: int = 0
+        self.text = text
+        self.position = 0
+        self.currentChar = self.text[self.position]
 
-    def NextToken(self) -> SyntaxToken:
-        """Detects <number>, <symbols> and <whitespaces>"""
+    def nextChar(self):
+        self.position += 1
+        self.currentChar = self.text[self.position] if self.position < len(self.text) else None
 
-        if self.__position >= len(self.text):
-            return SyntaxToken(SyntaxKind.EndOfFileToken, self.__position, "\0", None)
+    def getTokens(self):
+        tokens = []
 
-        if self.text[self.__position].isdigit():
-            start = self.__position
-            while self.text[self.__position].isdigit():
-                self.__position+=1
+        while self.currentChar != None:
+            if self.currentChar==" ":
+                start = self.position
+                while self.currentChar==" ":
+                    self.nextChar()
 
-            length = self.__position - start
-            text = self.text[start:start+length]
-            value = int(text)
-            return SyntaxToken(SyntaxKind.NumberToken, start, text, value)
+                length = self.position - start
+                text = self.text[start:start+length]
+                tokens.append(Token(start, TokenTypes.WhiteSpace, text))
 
-        if self.text[self.__position]==" ":
-            start = self.__position
-            while self.text[self.__position]==" ":
-                self.__position+=1
+            elif self.currentChar.isdigit():
+                start = self.position
+                while self.currentChar.isdigit() or self.currentChar==".":
+                    self.nextChar()
 
-            length = self.__position - start
-            text = self.text[start:start+length]
-            return SyntaxToken(SyntaxKind.WhiteSpaceToken, start, text, None)
+                length = self.position - start
+                text = self.text[start:start+length]
 
-        start = self.__position
-        self.__position+=1
+                value = float(text) if "." in text else int(text)
+                type_ = TokenTypes.Float if "." in text else TokenTypes.Int
+                tokens.append(Token(start, type_, text, value))
 
-        if self.text[start]=="+":
-            return SyntaxToken(SyntaxKind.PlusToken, start, "+", None)
-        elif self.text[start]=="-":
-            return SyntaxToken(SyntaxKind.MinusToken, start, "-", None)
-        elif self.text[start]=="*":
-            return SyntaxToken(SyntaxKind.StarToken, start, "*", None)
-        elif self.text[start]=="/":
-            return SyntaxToken(SyntaxKind.SlashToken, start, "/", None)
-        elif self.text[start]=="(":
-            return SyntaxToken(SyntaxKind.OpenParenthesisToken, start, "(", None)
-        elif self.text[start]==")":
-            return SyntaxToken(SyntaxKind.ClosedParenthesisToken, start, ")", None)
+            elif self.currentChar == "+":
+                tokens.append(Token(self.position, TokenTypes.Plus, "+"))
+                self.nextChar()
+            elif self.currentChar == "-":
+                tokens.append(Token(self.position, TokenTypes.Minus, "-"))
+                self.nextChar()
+            elif self.currentChar == "*":
+                tokens.append(Token(self.position, TokenTypes.Asterisk, "*"))
+                self.nextChar()
+            elif self.currentChar == "/":
+                tokens.append(Token(self.position, TokenTypes.Slash, "/"))
+                self.nextChar()
+            elif self.currentChar == "(":
+                tokens.append(Token(self.position, TokenTypes.LParenthesis, "("))
+                self.nextChar()
+            elif self.currentChar == ")":
+                tokens.append(Token(self.position, TokenTypes.RParenthesis, ")"))
+                self.nextChar()
+            else:
+                tokens.append(Token(self.position, TokenTypes.BadToken, ""))
+                self.nextChar()
 
-        return SyntaxToken(SyntaxKind.BadToken, start, self.text[start:self.__position], None)
+        return tokens
 
 
 with open("C:/Users/ndmch/Documents/Projects/Slow/stages/lexer/test.sl", "r") as file:
     fileLines = file.read().splitlines()
 
     for line in fileLines:
-        print(line)
         lexer = Lexer(line)
-        token = lexer.NextToken()
-        print(f"Kind: {token.kind}, Position: {token.position}, Text: {token.text}")
-
-        while token.kind != SyntaxKind.EndOfFileToken:
-            token = lexer.NextToken()
-            print(f"Kind: {token.kind}, Position: {token.position}, Text: {token.text}")
-
-        print("\n")
+        tokens = lexer.getTokens()
+        for token in tokens:
+            print(token.__dict__)
